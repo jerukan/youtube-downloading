22afa0c71c43c61a6c86f9c13968ad08131b3d39
Jerry Yan <jeyan@ucsd.edu>
2023-03-24 20:50:20 -0700

    Move around code

--- download_playlist_mp3.py
+++ download_playlist_mp3.py
@@ -38,12 +38,63 @@ def convert_to_mp3(inpath, outpath, artist=None, title=None, tnpath=None):
     audiofile.tag.save()
 
 
+def download_and_convert(url, outdir, overwrite):
+    # download from youtube
+    yt = YouTube(url)
+    title = yt.title
+    author = yt.author
+    # check if video from youtube music
+    from_ytmusic = False
+    if author[-7:] == "- Topic":
+        author = author[:-8]
+        from_ytmusic = True
+    # slashes screw with filepaths
+    author = author.replace("/", "")
+
+    print(f"Preparing {title} from {url}")
+    # YouTube audio streams have some issues where they download as twice the intended
+    # length, so we have to use ffmpeg to convert them to an mp3 in order to resolve
+    # those issues.
+    strm = yt.streams.filter(only_audio=True).first()
+    # very important to use since we need valid filenames
+    filename = strm.default_filename
+    dlpath = outdir / filename
+    mp3_name = f"{dlpath.stem} - {author}.mp3"
+    mp3_path = outdir / mp3_name
+    if mp3_path.exists() and not overwrite:
+        print(f"Audio file {mp3_path} already exists. Skipping...")
+        return
+    # grab thumbnail
+    thumbnail_path = outdir / f"{dlpath.stem}.jpeg"
+    response = requests.get(yt.thumbnail_url)
+    img = Image.open(BytesIO(response.content))
+    # thumbnails are 640 x 480
+    if from_ytmusic:
+        img = img.crop((140, 60, 140 + 360, 60 + 360))
+    else:
+        img = img.crop((0, 60, 640, 60 + 360))
+    img.thumbnail((300, 300))
+    img.save(thumbnail_path, "JPEG")
+    # download audio mp4
+    print("Downloading from stream...")
+    strm.download(outdir)
+    print(f"{dlpath} successfully downloaded.")
+
+    # convert to mp3 with ffmpeg
+    thdfunc = lambda: convert_to_mp3(dlpath, mp3_path, artist=author, title=title, tnpath=thumbnail_path)
+    thd = threading.Thread(target=thdfunc)
+    thd.start()
+
+
 def main():
     parser = argparse.ArgumentParser(description="Downloads a YouTube playlist as mp3 files to a specified directory.")
     parser.add_argument("playlist", type=str, help="YouTube link to a public/unlisted playlist")
-    parser.add_argument("overwrite", type=bool, help="If true, overwrite existing mp3 files", nargs="?", const=True, default=True)
+    parser.add_argument("--overwrite", action=argparse.BooleanOptionalAction)
 
     args = parser.parse_args()
+    overwrite = args.overwrite
+    if overwrite is None:
+        overwrite = False
 
     pl = Playlist(args.playlist)
     pltitle = pl.title
@@ -55,48 +106,7 @@ def main():
     while len(vidqueue) > 0:
         url = vidqueue.pop(0)
         try:
-            # download from youtube
-            yt = YouTube(url)
-            title = yt.title
-            author = yt.author
-            # check if video from youtube music
-            from_ytmusic = False
-            if author[-7:] == "- Topic":
-                author = author[:-8]
-                from_ytmusic = True
-
-            print(f"Preparing {title} from {url}")
-            # YouTube audio streams have some issues where they download as twice the intended
-            # length, so we have to use ffmpeg to convert them to an mp3 in order to resolve
-            # those issues.
-            strm = yt.streams.filter(only_audio=True).first()
-            # very important to use since we need valid filenames
-            filename = strm.default_filename
-            dlpath = outdir / filename
-            # grab thumbnail
-            thumbnail_path = outdir / f"{dlpath.stem}.jpeg"
-            response = requests.get(yt.thumbnail_url)
-            img = Image.open(BytesIO(response.content))
-            # thumbnails are 640 x 480
-            if from_ytmusic:
-                img = img.crop((140, 60, 140 + 360, 60 + 360))
-            else:
-                img = img.crop((0, 60, 640, 60 + 360))
-            img.thumbnail((300, 300))
-            img.save(thumbnail_path, "JPEG")
-            mp3_name = f"{dlpath.stem} - {author}.mp3"
-            mp3_path = outdir / mp3_name
-            if mp3_path.exists() and not args.overwrite:
-                print(f"Audio file {mp3_path} already exists. Skipping...")
-                continue
-            print("Downloading from stream...")
-            strm.download(outdir)
-            print(f"{dlpath} successfully downloaded.")
-
-            # convert to mp3 with ffmpeg
-            convfunc = lambda: convert_to_mp3(dlpath, mp3_path, artist=author, title=title, tnpath=thumbnail_path)
-            conv_thr = threading.Thread(target=convfunc)
-            conv_thr.start()
+            download_and_convert(url, outdir, overwrite)
         except Exception as e:
             traceback.print_exc()
             print(f"Failed to download {url}, will retry at the end...")
